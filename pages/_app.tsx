43bb540c3b60a4a43f8835b737f4f3e695ef8a68
Dalufishe <fishawe87@gmail.com>
2023-07-21 02:40:05 +0800

    feat: vercel-analysis

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,18 @@ import "../styles/global.scss";
 import type { AppProps } from "next/app";
 import { store, persistor } from "../redux/store";
 import { PersistGate } from "redux-persist/integration/react";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <PersistGate persistor={persistor}>
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
-    </PersistGate>
+    <>
+      <PersistGate persistor={persistor}>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </PersistGate>
+      <Analytics />
+    </>
   );
 }
 
