609e351fd152a22d5188fdf09de111bab1645b5b
Dalufishe <fishawe87@gmail.com>
2024-04-07 00:53:34 +0800

    haha

--- pages/fdjakfjdakfvjkajk/index.tsx
+++ pages/fdjakfjdakfvjkajk/index.tsx
@@ -26,10 +26,7 @@ export default function Page() {
               height="630"
               src="https://www.youtube.com/embed/E-Gv7YvVIO4?si=WksqiS4OsTiRKGHj"
               title="YouTube video player"
-       
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerpolicy="strict-origin-when-cross-origin"
-              allowfullscreen
             ></iframe>
             <div
               className="w-full h-full absolute top-0 z-10"
@@ -44,10 +41,7 @@ export default function Page() {
             height="630"
             src="https://www.youtube.com/embed/GtL1huin9EE?si=236Qme2bYut_ntZk&rel=0&amp;autoplay=1"
             title="YouTube video player"
-       
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin"
-            allowfullscreen
           ></iframe>
         )}
       </div>
