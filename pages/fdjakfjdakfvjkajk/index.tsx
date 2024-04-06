6bcd6bc66b225985b8b0c45cbacbdb565f8019d0
Dalufishe <fishawe87@gmail.com>
2024-04-07 00:51:56 +0800

    haha

--- pages/fdjakfjdakfvjkajk/index.tsx
+++ pages/fdjakfjdakfvjkajk/index.tsx
@@ -26,7 +26,7 @@ export default function Page() {
               height="630"
               src="https://www.youtube.com/embed/E-Gv7YvVIO4?si=WksqiS4OsTiRKGHj"
               title="YouTube video player"
-              frameborder="0"
+       
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               referrerpolicy="strict-origin-when-cross-origin"
               allowfullscreen
@@ -44,7 +44,7 @@ export default function Page() {
             height="630"
             src="https://www.youtube.com/embed/GtL1huin9EE?si=236Qme2bYut_ntZk&rel=0&amp;autoplay=1"
             title="YouTube video player"
-            frameborder="0"
+       
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerpolicy="strict-origin-when-cross-origin"
             allowfullscreen
